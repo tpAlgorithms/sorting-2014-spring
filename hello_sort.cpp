b7564d713cf569454bcd374e5901c005e44d9b85
Georgy Ivanov <ivanov.georg@gmail.com>
2014-04-12 09:59:01 +0400

    sync

--- hello_sort.cpp
+++ hello_sort.cpp
@@ -1,48 +1,65 @@
 #include <stdint.h>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 typedef int* iterator_t;
 
-void sort(iterator_t &begin, size_t size);
+struct less_t {
+  bool operator() (const int& lhs, const int& rhs) const {
+    return lhs > rhs;
+  }
+};
 
-void sort(iterator_t &begin, iterator_t &end) {
-  size_t length = 0;
-  for (iterator_t it = begin; it != end; ++it) { ++length; }
 
-  //length = std::distance(begin, end);
-
-  sort(begin, length);
+bool less_func(const int& lhs, const int& rhs) {
+  return lhs > rhs;
 }
 
-void sort(iterator_t &begin, size_t size) {
+//(bool)less_func_t(const int& lhs, const int& rhs);
+//typedef less_func_t less_t;
+
+//template <typename less_type>
+//void sort(iterator_t &begin, size_t size, less_type const &less);
+
+template <typename less_type>
+void sort(iterator_t begin, iterator_t end, less_type const &less) {
+  std::sort(begin, end, less);
+}
+
+/*
+template <typename less_type>
+void sort(iterator_t &begin, size_t size, less_t const &less) {
   iterator_t end = begin;
   for (size_t i = 0; i < size; ++i) { 
     ++end;
 //    end = it + 1;
   }
 
-  std::sort(begin, end);
+  std::sort(begin, end, less);
 }
-
+*/
 
 
 int main() {
   
-  size_t size;
-  std::cin >> size;
+  std::vector<int> data;
 
-  int *data = new int[size];
+//  while (1) {
+  for(;;) {    
+    int value;
+    std::cin >> value;
 
-  for (size_t i = 0; i < size; ++i) {
-    std::cin >> data[i];
+    if (std::cin.eof()) break;
+    data.push_back(value);
   }
 
+  less_t less;
 
-  std::sort(data, &data[size]);
+  sort<less_t>(&data[0], &data[data.size()], less);
 
 
-  for (size_t i = 0; i < size; ++i) {
+  for (size_t i = 0; i < data.size(); ++i) {
     std::cout << data[i] << std::endl;
   }
 
